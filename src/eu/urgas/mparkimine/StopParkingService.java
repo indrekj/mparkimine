4212bb3f523fd4d9e284a3a9c2c35c2e41d58531
Indrek Juhkam <indrek@urgas.eu>
2013-07-08 23:40:52 +0300

    Do not offer app when stopping parking

--- src/eu/urgas/mparkimine/StopParkingService.java
+++ src/eu/urgas/mparkimine/StopParkingService.java
@@ -1,7 +1,12 @@
 package eu.urgas.mparkimine;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.*;
 import android.net.Uri;
+import android.util.Log;
+
+import java.util.List;
 
 class StopParkingService {
     private final MyApp app;
@@ -17,6 +22,18 @@ class StopParkingService {
         Intent callIntent = new Intent(Intent.ACTION_CALL);
         callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         callIntent.setData(Uri.parse("tel:" + ParkingManager.PARKING_OPERATOR_STOP_NUMBER));
+
+
+        List<ResolveInfo> resolveInfos = app.getPackageManager().queryIntentActivities
+                (callIntent, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT);
+
+        if (resolveInfos.size() > 0) {
+            ActivityInfo activityInfo = resolveInfos.get(0).activityInfo;
+            String appPackage = activityInfo.applicationInfo.packageName;
+            String appName = activityInfo.name;
+            callIntent.setComponent((new ComponentName(appPackage, appName)));
+        }
+
         app.startActivity(callIntent);
     }
 }
